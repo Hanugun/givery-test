3fe00419eac9b4d70d22bc88fd68ea2f5d8ed257
HanugunNodamen <86820221+HanugunNodamen@users.noreply.github.com>
2024-09-11 01:51:07 +0900

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,10 +15,15 @@ import java.sql.Timestamp
 import slick.jdbc.MySQLProfile.api._
 import java.text.SimpleDateFormat
 
-// Case class for RecipeData
-case class RecipeData(title: String, makingTime: String, serves: String, ingredients: String, cost: Int)
+case class RecipeData(
+  title: Option[String], 
+  makingTime: Option[String], 
+  serves: Option[String], 
+  ingredients: Option[String], 
+  cost: Option[Int]
+)
 
-// Companion object for JSON formatting using Play JSON
+
 object RecipeData {
   implicit val recipeDataFormat: OFormat[RecipeData] = Json.format[RecipeData]
 }
@@ -59,45 +64,75 @@ object PlayJsonSupport {
     }
 }
 
+
 object Main extends App with JsonFormats {
 
   implicit val system = ActorSystem("my-system")
   implicit val executionContext = system.dispatcher
 
-  // Sample data for testing
   val db = Database.forConfig("mydb")
   val recipeRepository = new RecipeRepository(db)
 
   import PlayJsonSupport._
+  def validateRecipeData(recipeData: RecipeData): Option[String] = {
+    if (recipeData.title.isEmpty) Some("Title is required")
+    else if (recipeData.makingTime.isEmpty) Some("Making time is required")
+    else if (recipeData.serves.isEmpty) Some("Serves is required")
+    else if (recipeData.ingredients.isEmpty) Some("Ingredients are required")
+    else if (recipeData.cost.isEmpty || recipeData.cost.exists(_ <= 0)) Some("Cost must be a positive integer")
+    else None
+  }
 
-  // Route definition
   val route: Route =
+  handleExceptions(myExceptionHandler) {
     path("recipes" / IntNumber) { id =>
       get {
         onSuccess(recipeRepository.getRecipeById(id)) {
           case Some(recipe) => {
-              val jsonResponse = Json.obj(
-                "message" -> JsString("Recipe details by id"),
-                "recipes" -> Json.toJson(recipe)
-              )
-              complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+            val jsonResponse = Json.obj(
+              "message" -> JsString("Recipe details by id"),
+              "recipes" -> Json.toJson(recipe)
+            )
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
           }
-          case None => complete(s"Recipe with ID $id not found")
+          case None => {
+            val jsonResponse = Json.obj(
+                "message" -> JsString(s"Recipe with ID $id not found"),
+              )
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+          }
         }
       } ~
       patch {
         entity(as[RecipeData]) { recipeData =>
-          val updateRecipe = Recipe(None, recipeData.title, recipeData.makingTime, recipeData.serves, recipeData.ingredients, recipeData.cost)
-          onComplete(recipeRepository.updateRecipe(id, updateRecipe)) { 
-            case scala.util.Success(id) =>
-              val jsonResponse = Json.obj(
-                "message" -> JsString("Recipe successfully updated!"),
-                "recipe" -> Json.toJson(updateRecipe)
+          validateRecipeData(recipeData) match {
+            case Some(errorMessage) =>
+              val errorResponse = Json.obj(
+                "message" -> JsString("Recipe update failed!"),
+                "required" -> JsString("title, makingTime, serves, ingredients, cost")
               )
-              complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
-            case scala.util.Failure(exception) =>
-              val jsonResponse = Json.obj("message" -> JsString("No recipe found!"))
-              complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+              complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`application/json`, errorResponse.toString()))
+
+            case None =>
+              val updateRecipe = Recipe(
+                None, 
+                recipeData.title.getOrElse(""),
+                recipeData.makingTime.getOrElse(""), 
+                recipeData.serves.getOrElse(""), 
+                recipeData.ingredients.getOrElse(""), 
+                recipeData.cost.getOrElse(0)
+              )
+              onComplete(recipeRepository.updateRecipe(id, updateRecipe)) { 
+                case scala.util.Success(id) =>
+                  val jsonResponse = Json.obj(
+                    "message" -> JsString("Recipe successfully updated!"),
+                    "recipe" -> Json.toJson(updateRecipe)
+                  )
+                  complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+                case scala.util.Failure(exception) =>
+                  val jsonResponse = Json.obj("message" -> JsString("No recipe found!"))
+                  complete(StatusCodes.NotFound, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+              }
           }
         }
       } ~
@@ -105,10 +140,10 @@ object Main extends App with JsonFormats {
         onComplete(recipeRepository.deleteRecipe(id)) {
           case scala.util.Success(id) =>
             val jsonResponse = Json.obj("message" -> JsString("Recipe successfully removed!"))
-            complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
           case scala.util.Failure(exception) =>
             val jsonResponse = Json.obj("message" -> JsString("No recipe found!"))
-            complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+            complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
         }
       }
     } ~
@@ -116,40 +151,61 @@ object Main extends App with JsonFormats {
       get {
         onSuccess(recipeRepository.getAllRecipes) { recipes =>
           val jsonResponse = Json.obj("recipes" -> Json.toJson(recipes))
-          complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+          complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
         }
       } ~
       post {
         entity(as[RecipeData]) { recipeData =>
-          val currentTimestamp = new Timestamp(System.currentTimeMillis())
-          val newRecipe = Recipe(None, recipeData.title, recipeData.makingTime, recipeData.serves, recipeData.ingredients, recipeData.cost, Some(currentTimestamp), Some(currentTimestamp))
-          onComplete(recipeRepository.insertRecipe(newRecipe)) {
-            case scala.util.Success(id) =>
-              val savedRecipe = newRecipe.copy(id = Some(id))
-              val jsonResponse = Json.obj(
-                "message" -> JsString("Recipe successfully created!"),
-                "recipe" -> Json.toJson(savedRecipe)
-              )
-              complete(HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
-
-            case scala.util.Failure(exception) =>
+          validateRecipeData(recipeData) match {
+            case Some(errorMessage) =>
               val errorResponse = Json.obj(
                 "message" -> JsString("Recipe creation failed!"),
-                "required" -> JsString("title, makingTime, serves, ingredients, cost"),
+                "required" -> JsString("title, makingTime, serves, ingredients, cost")
               )
-              complete(StatusCodes.InternalServerError, HttpEntity(ContentTypes.`application/json`, errorResponse.toString()))
+              complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`application/json`, errorResponse.toString()))
+
+            case None =>
+              val currentTimestamp = new Timestamp(System.currentTimeMillis())
+              val newRecipe = Recipe(
+                None,
+                recipeData.title.getOrElse(""),
+                recipeData.makingTime.getOrElse(""),
+                recipeData.serves.getOrElse(""),
+                recipeData.ingredients.getOrElse(""),
+                recipeData.cost.getOrElse(0),
+                Some(currentTimestamp), 
+                Some(currentTimestamp)
+              )
+
+              onComplete(recipeRepository.insertRecipe(newRecipe)) {
+                case scala.util.Success(id) =>
+                  val savedRecipe = newRecipe.copy(id = Some(id))
+                  val jsonResponse = Json.obj(
+                    "message" -> JsString("Recipe successfully created!"),
+                    "recipe" -> Json.toJson(savedRecipe)
+                  )
+                  complete(StatusCodes.OK, HttpEntity(ContentTypes.`application/json`, jsonResponse.toString()))
+
+                case scala.util.Failure(exception) =>
+                  val errorResponse = Json.obj(
+                    "message" -> JsString("Recipe creation failed!"),
+                    "required" -> JsString("title, makingTime, serves, ingredients, cost")
+                  )
+                  complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`application/json`, errorResponse.toString()))
+              }
           }
         }
-      }
+      } 
     } ~
     complete(StatusCodes.NotFound, "The requested resource could not be found.")
+  }
     
 
   // Start the server
   val port = sys.env.getOrElse("PORT", "8080").toInt
   val bindingFuture = Http().newServerAt("0.0.0.0", port).bind(route)
 
-  println(s"Server online at http://localhost:$port/")
+  println(s"Server online at http://0.0.0.0:$port/")
 
   Await.result(system.whenTerminated, Duration.Inf)
 }
